c8e238f776cb9506b627af82c9f18536a2cbd369
Christian Fox <christianfox890@icloud.com>
2016-10-18 18:34:39 +0100

    bumped to v0.2.0

--- Example/Pods/Target Support Files/KFXUtilities/KFXUtilities-umbrella.h
+++ Example/Pods/Target Support Files/KFXUtilities/KFXUtilities-umbrella.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
+#import "KFXGeoLocationHelper.h"
+#import "KFXMaths.h"
 
 FOUNDATION_EXPORT double KFXUtilitiesVersionNumber;
 FOUNDATION_EXPORT const unsigned char KFXUtilitiesVersionString[];
