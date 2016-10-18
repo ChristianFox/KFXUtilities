a006931d6daa71b55f2963b360e5ea5c1b9058f6
Christian Fox <christianfox890@icloud.com>
2016-10-18 18:03:50 +0100

    Initial commit

---
+++ Example/KFXUtilities/DEMOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEMOAppDelegate.h
+//  KFXUtilities
+//
+//  Created by Christian Fox on 10/18/2016.
+//  Copyright (c) 2016 Christian Fox. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DEMOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/KFXUtilities/DEMOViewController.h
@@ -0,0 +1,13 @@
+//
+//  DEMOViewController.h
+//  KFXUtilities
+//
+//  Created by Christian Fox on 10/18/2016.
+//  Copyright (c) 2016 Christian Fox. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DEMOViewController : UIViewController
+
+@end
